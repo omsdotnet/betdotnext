bc6ee8561ea5f15bcc9760e7be57deaf50033325
Oleg Peresada <peresada.oleg@mail.ru>
2019-10-19 01:06:55 +0600

    environment

--- BetDotNext/BetDotNext/Startup.cs
+++ BetDotNext/BetDotNext/Startup.cs
@@ -3,6 +3,7 @@ using BetDotNext.Services;
 using BetDotNext.Setup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
 using Telegram.Bot;
@@ -11,22 +12,33 @@ namespace BetDotNext
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = string.Empty;
-            var database = string.Empty;
-            var adminPass = string.Empty;
-            var userPass = string.Empty;
+            var connection = _configuration["Mongo"];
+            var database = _configuration["DB"];
 
-            var telegramToken = string.Empty;
+            var telegramToken = _configuration["TelegramToken"];
 
             services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(telegramToken));
             services.AddSingleton(_ => new MongoClient(connection).GetDatabase(database).MongoDbInit());
-            
+
             services.AddSingleton<BetService>();
             services.AddSingleton<QueueMessagesService>();
             services.AddSingleton<ActiveCommandService>();
-            
+
             services.AddHostedService<BetToTelegramService>();
 
             services.AddSingleton<UserRepository>();
