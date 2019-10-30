6fe7dc15afc5a63f0dc3724d6fe81eb26632a56a
Oleg Peresada <peresada.oleg@mail.ru>
2019-10-31 00:50:27 +0600

    fix

--- BetDotNext/BetDotNext/Program.cs
+++ BetDotNext/BetDotNext/Program.cs
@@ -12,7 +12,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
+using Serilog;
+using Serilog.Events;
 using Telegram.Bot;
 
 namespace BetDotNext
@@ -21,13 +24,36 @@ namespace BetDotNext
   {
     private static IConfiguration _configuration;
 
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      try
+      {
+        Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+          .Enrich.FromLogContext()
+          .WriteTo.Console()
+          .WriteTo.Seq("http://192.168.168.131:4000")
+          .CreateLogger();
+
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception ex)
+      {
+        Log.Fatal(ex, "Host terminated unexpectedly");
+        return 1;
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
+
+      return 0;
     }
 
     private static IHostBuilder CreateHostBuilder(string[] args) =>
       Host.CreateDefaultBuilder(args)
+        .ConfigureLogging(ConfigureLogging)
         .ConfigureAppConfiguration((hostingContext, builder) =>
         {
           builder.SetBasePath(Directory.GetCurrentDirectory())
@@ -41,7 +67,7 @@ namespace BetDotNext
           {
             var connection = _configuration["Mongo"] ?? "mongodb://192.168.168.131:28017";
             var database = _configuration["DB"] ?? "zx";
-            var telegramToken = _configuration["TelegramToken"] ?? "606619300:AAFdo1oRuERSg-CEtoik5D198BrRV2gPrtM";
+            var telegramToken = _configuration["TelegramToken"] ?? "606619300:AAErpzzU1A1LtArae57jrvYJbtXWAKR087M";
 
             var handler = new HttpClientHandler
             {
@@ -76,7 +102,12 @@ namespace BetDotNext
               .AddActivity("/removebet", typeof(RemoveBetActivity));
 
             app.ApplicationServices.GetRequiredService<BetService>().Start();
-          });
+          }).UseSerilog();
         });
+
+    private static void ConfigureLogging(HostBuilderContext arg1, ILoggingBuilder arg2)
+    {
+      var host = arg1.Configuration["hot"];
+    }
   }
 }
