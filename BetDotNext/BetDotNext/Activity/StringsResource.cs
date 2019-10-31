13efd904643bbae495353de3a4009ed31a88d32a
Pavel <kulbida@hotmail.com>
2019-11-01 01:46:43 +0600

    messages tune

--- BetDotNext/BetDotNext/Activity/StringsResource.cs
+++ BetDotNext/BetDotNext/Activity/StringsResource.cs
@@ -2,15 +2,22 @@
 {
   public static class StringsResource
   {
-    public static string StartActivityMessage = "Привет!!!. Я бот который принимает ставки на DotNext Moscow 2019.";
+    public static string StartActivityMessage = "Привет!!!. Я бот который принимает ставки на рейтинг спикеров DotNext\r\n" +
+                                                "Результаты ставок: http://bookmakerboard.azurewebsites.net/ \r\n" +
+                                                "Спикеры: https://dotnext-moscow.ru/people/";
 
-    public static readonly string AcceptedBetActivityMessage = "Формат принятия ставки следующий: \r\n[спикер]-[ставка]-[номинация]";
+    public static readonly string AcceptedBetActivityMessage = "Формат принятия ставки следующий: \r\n" +
+                                                               "[спикер]-[ставка]-[номинация] \r\n" +
+                                                               "Например:\r\n" +
+                                                               "Christophe Nasarre - 300 - 1\r\n" +
+                                                               "Роман Просин - 100 - top1\r\n" +
+                                                               "tukasz Pyrzyk  - 10 - top10";
 
-    public static readonly string LoadingMessage = "Ща как поставлю. Ты красавчик!!!";
+    public static readonly string LoadingMessage = "Обрабатываю Вашу ставку...";
 
     public static readonly string BetActivityUnexpectedFormatMessage = "Не верный формат ставки";
 
-    public static readonly string SuccessBetActivity = "Ставка успешно принята. \r\nТекущее количество очков {0}.";
+    public static readonly string SuccessBetActivity = "Ставка успешно принята. \r\nТекущее количество баллов {0}.";
 
     public static readonly string FailCreatedActivityMessage = "Не удалось создать ставку.\r\nПопробуйте выполнить команду снова.";
 
