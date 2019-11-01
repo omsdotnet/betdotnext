17fd5928ca818cb9650736e973e61f9402444f62
Oleg Peresada <peresada.oleg@mail.ru>
2019-11-02 01:02:32 +0600

    k

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ ls -lR
 echo 'decrypt file key'
 curl -sflL 'https://raw.githubusercontent.com/appveyor/secure-file/master/install.sh' | bash -e -
 ./appveyor-tools/secure-file -decrypt keys/key.enc -secret $secret -salt $salt
-if [[ $? -ne 0 ]]; then
+if [ $? -ne 0 ]; then
     echo 'secure-file fail'
     exit 1
 fi
 
 echo 'substitute docker-compose.yml'
 envsubst < 'docker-compose.prod.yml' > 'docker-compose.yml'
-if [[ $? -ne 0 ]]; then
+if [ $? -ne 0 ]; then
     echo 'envsubst fail'
     exit 1
 fi
