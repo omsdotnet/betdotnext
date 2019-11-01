de39623524c34c9e5f3ef4e05fb603abb62788ed
Oleg Peresada <peresada.oleg@mail.ru>
2019-11-02 00:39:28 +0600

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 
+ls -lr
+
 echo 'decrypt file key'
 ./appveyor-tools/secure-file -decrypt /keys/key.enc -secret $secret -salt $salt
+if [[ $? -ne 0 ]]; then
+echo 'secure-file fail'
+exit 1
 
 echo 'substitute docker-compose.yml'
 envsubst < 'docker-compose.prod.yml' > 'docker-compose.yml'
+if [[ $? -ne 0 ]]; then
+echo 'envsubst fail'
+exit 1
 
-ssh -i /keys/key.enc root@$server
+ssh -i /keys/key root@$server
