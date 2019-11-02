5105e7732f02099755889ffbd76c5a7db0346792
Pavel <kulbida@hotmail.com>
2019-11-02 10:57:38 +0600

    messages tune

--- BetDotNext/BetDotNext/Activity/StringsResource.cs
+++ BetDotNext/BetDotNext/Activity/StringsResource.cs
@@ -3,30 +3,33 @@
   public static class StringsResource
   {
     public static string StartActivityMessage = "Привет!!!. Я бот который принимает ставки на рейтинг спикеров DotNext\r\n" +
-                                                "Результаты ставок: http://bookmakerboard.azurewebsites.net/ \r\n" +
-                                                "Спикеры: https://dotnext-moscow.ru/people/";
+                                                "Спикеры: https://dotnext-moscow.ru/people/ \r\n" +
+                                                "Результаты ставок: http://bookmakerboard.azurewebsites.net/";
 
     public static readonly string AcceptedBetActivityMessage = "Формат принятия ставки следующий: \r\n" +
                                                                "[спикер]-[ставка]-[номинация] \r\n" +
                                                                "Например:\r\n" +
                                                                "Christophe Nasarre - 300 - 1\r\n" +
                                                                "Роман Просин - 100 - top3\r\n" +
-                                                               "tukasz Pyrzyk  - 10 - top10";
+                                                               "tukasz Pyrzyk - 10 - top10";
 
     public static readonly string LoadingMessage = "Обрабатываю Вашу ставку...";
 
     public static readonly string BetActivityUnexpectedFormatMessage = "Не верный формат ставки";
 
-    public static readonly string SuccessBetActivity = "Ставка успешно принята. \r\nТекущее количество баллов {0}.";
+    public static readonly string SuccessBetActivity = "Ставка успешно принята.\r\n" + 
+                                                       "Текущее количество баллов {0}.";
 
-    public static readonly string FailCreatedActivityMessage = "Не удалось создать ставку.\r\nПопробуйте выполнить команду снова.";
+    public static readonly string FailCreatedActivityMessage = "Не удалось создать ставку.\r\n" + 
+                                                               "Попробуйте выполнить команду снова.";
 
-    public static readonly string FailDeleteActivityMessage = "Не удалось удалить ставку.\r\nПопробуйте выполнить команду снова.";
+    public static readonly string FailDeleteActivityMessage = "Не удалось удалить ставку.\r\n" + 
+                                                              "Попробуйте выполнить команду снова.";
 
     public static readonly string RemoveBetActivityMessage = "Данная команда предпологает следующий формат снятия ставки:\r\n" + 
-                                                             "[участник]-[спикер]-0-[номинация]\r\n" +
-                                                             "[участник]-[спикер]-0 (снять все ставки на спикера)\r\n" +
-                                                             "[участник]-0 (снять все текущие ставки участника)";
+                                                             "[спикер]-0-[номинация]\r\n" +
+                                                             "[спикер]-0 (снять все ставки на спикера)\r\n" +
+                                                             "0 (снять все текущие ставки участника)";
 
     public static readonly string BetRateNotEquelsMessage = "Ставка должна быть равна 0.";
 
