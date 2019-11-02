820ef223b5c67cc2ed9bc87372333560c8f1c89e
Oleg Peresada <peresada.oleg@mail.ru>
2019-11-03 01:11:02 +0600

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+export TELEGRAM_TOKEN=$TELEGRAM_TOKEN
+export DB=$DB
+
 echo 'substitute docker-compose.yml'
 envsubst < 'docker-compose.prod.yml' > 'docker-compose.yml'
 if [ $? -ne 0 ]; then
     echo 'envsubst fail'
     exit 1
 fi
-
-export TELEGRAM_TOKEN=$TELEGRAM_TOKEN
-export DB=$DB
 
 ./rdeploy/rdeploy --username root --host $server --private-key keys/key --passphrase $code --source docker-compose.yml --destination "/home/dotnext/docker-compose.yml"
 ./rdeploy/rdeploy --username root --host $server --private-key keys/key --passphrase $code --command "docker-compose -f /home/dotnext/docker-compose.yml up -d"
