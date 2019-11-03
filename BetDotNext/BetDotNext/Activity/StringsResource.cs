378c909ad42a263c3af8c6a60fa6b0eef5edbad4
Pavel <kulbida@hotmail.com>
2019-11-03 21:45:23 +0600

    messages tune

--- BetDotNext/BetDotNext/Activity/StringsResource.cs
+++ BetDotNext/BetDotNext/Activity/StringsResource.cs
@@ -2,7 +2,7 @@
 {
   public static class StringsResource
   {
-    public static string StartActivityMessage = "Привет! Я бот, который принимает ставки на рейтинг спикеров DotNext\r\n" +
+    public static string StartActivityMessage = "Привет! Я бот, который принимает ставки на рейтинг спикеров DotNext.\r\n" +
                                                 "Спикеры: https://dotnext-moscow.ru/people/ \r\n" +
                                                 "Результаты ставок: http://bookmakerboard.azurewebsites.net/";
 
@@ -13,11 +13,11 @@
                                                                "Роман Просин - 100 - top3\r\n" +
                                                                "tukasz Pyrzyk - 10 - top10";
 
-    public static readonly string LoadingMessage = "Обрабатываю Вашу ставку...";
+    public static readonly string LoadingMessage = "Обрабатываю Вашу команду...";
 
     public static readonly string BetActivityUnexpectedFormatMessage = "Не верный формат ставки";
 
-    public static readonly string SuccessBetActivity = "Ставка успешно принята.\r\n" + 
+    public static readonly string SuccessBetActivity = "Команда успешно обработана.\r\n" + 
                                                        "Текущее количество баллов {0}";
 
     public static readonly string FailCreatedActivityMessage = "Не удалось создать ставку.\r\n" + 
@@ -28,8 +28,7 @@
 
     public static readonly string RemoveBetActivityMessage = "Данная команда предпологает следующий формат снятия ставки:\r\n" + 
                                                              "[спикер] - 0 - [номинация]\r\n" +
-                                                             "[спикер] - 0 (снять все ставки на спикера)\r\n" +
-                                                             "0 (снять все текущие ставки участника)";
+                                                             "[спикер] - 0 (снять все ставки на спикера)";
 
     public static readonly string BetRateNotEquelsMessage = "Ставка должна быть равна 0.";
 
