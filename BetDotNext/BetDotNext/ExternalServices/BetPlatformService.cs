3374de0ae61d2ff246d974d70163220e9c6079b6
Pavel <kulbida@hotmail.com>
2019-11-06 03:22:45 +0600

    debug

--- BetDotNext/BetDotNext/ExternalServices/BetPlatformService.cs
+++ BetDotNext/BetDotNext/ExternalServices/BetPlatformService.cs
@@ -107,7 +107,7 @@ namespace BetDotNext.ExternalServices
 
       var teams = await TeamsAsync();
 
-      var speaker = teams.SingleOrDefault(x => x.Name.Replace('-', ' ').Equals(bet.Speaker, StringComparison.OrdinalIgnoreCase));
+      var speaker = teams.SingleOrDefault(x => x.Name.Replace('-', ' ').Equals(bet.Speaker, StringComparison.InvariantCultureIgnoreCase));
       if (speaker == null)
       {
         _logger.LogError("Not found speaker");
@@ -184,7 +184,7 @@ namespace BetDotNext.ExternalServices
       }
 
       var teams = await TeamsAsync();
-      var speaker = teams.SingleOrDefault(x => x.Name.Replace('-', ' ').Equals(bet.Speaker, StringComparison.OrdinalIgnoreCase));
+      var speaker = teams.SingleOrDefault(x => x.Name.Replace('-', ' ').Equals((bet.Speaker, StringComparison.InvariantCultureIgnoreCase));
       if (speaker == null)
       {
         _logger.LogError("ERROR - speaker not found");
